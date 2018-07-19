1dc6d10b11655c54c3ff9fd751de9b7346070d9e
Connor Jarvis <connor@lyno.in>
2018-07-19 18:53:02 -0400

    Cleaned up login and updated readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # MessengerToSMS
 
+This will relay messages from a Facebook Messenger group chat to SMS and vice versa. 
+
+Born out of a need to be able to have a group conversation with one party having no access to the Internet but with the ability to receive text messages.
+
 Theoretically all you need to do to get started as far as I know:
 - `Fill out config.example.js and save as config.js`
 - `npm install`
-- `node login.js`
-- `node index.js`
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var config= require('./config');
 var express = require('express');
 var app = express()
 const bodyParser = require('body-parser');
+const fs = require("fs");
+const login = require("facebook-chat-api");
 const client = require('twilio')(config.twilioSid, config.twilioToken);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/webhook', function (req, res) {
@@ -34,38 +37,60 @@ function sendFacebookMessage(body) {
     global.api.sendMessage(config.relayingFor + ": " + body,config.threadID)
 }
 
-const fs = require("fs");
-const login = require("facebook-chat-api");
-global.api = null
 
-login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
+function facebookLogin() {
 
-    api.setOptions({listenEvents: true});
-    global.api = api
-
-    var stopListening = api.listen((err, event) => {
-        if(err) return console.error(err);
-
-        api.markAsRead(event.threadID, (err) => {
-            if(err) console.error(err);
-        });
-
-        switch(event.type) {
-            case "message":
-                api.getUserInfo(event.senderID, (err, ret) => {
-                    if(err) return console.error(err);
-                    for(var prop in ret) {
-                        if(ret.hasOwnProperty(prop) && ret[prop].firstName) {
-                            sendSMSMessage(event.body,ret[prop].firstName)
-                        }
-                    }
-                });
-                break;
-            case "event":
-                console.log(event);
-                break;
+    return new Promise(function(resolve,reject){
+        if (fs.existsSync('appstate.json')) {
+            resolve(JSON.parse(fs.readFileSync('appstate.json', 'utf8')))
+        } else {
+            login({email: config.facebookUsername , password: config.facebookPassword}, (err, api) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+                    resolve(api.getAppState())
+                }
+            });
         }
-    });
-});
 
+    })
+}
+
+
+function main(){
+    var initalizeFacebookLogin = facebookLogin()
+    initalizeFacebookLogin.then(function(result) {
+        login({appState: result}, (err, api) => {
+            if(err) return console.error(err);
+        
+            api.setOptions({listenEvents: true});
+            global.api = api
+        
+            var stopListening = api.listen((err, event) => {
+                if(err) return console.error(err);
+        
+                api.markAsRead(event.threadID, (err) => {
+                    if(err) console.error(err);
+                });
+        
+                switch(event.type) {
+                    case "message":
+                        api.getUserInfo(event.senderID, (err, ret) => {
+                            if(err) return console.error(err);
+                            for(var prop in ret) {
+                                if(ret.hasOwnProperty(prop) && ret[prop].firstName) {
+                                    sendSMSMessage(event.body,ret[prop].firstName)
+                                }
+                            }
+                        });
+                        break;
+                }
+            });
+        });
+    }, function(err){
+        console.log(err)
+    })
+}
+
+main()
